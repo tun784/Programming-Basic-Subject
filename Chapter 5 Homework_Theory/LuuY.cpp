04b5300061c4479631a947de3338025ef86c5efe
Nguyen Hoang Tuan <116370663+tun784@users.noreply.github.com>
2023-07-07 15:45:43 +0700

    Update LuuY.cpp

--- Chapter 5 Homework_Theory/LuuY.cpp
+++ Chapter 5 Homework_Theory/LuuY.cpp
@@ -1,34 +1,34 @@
-//#include <stdio.h>
-//#include <conio.h>
-//void LuuYso()
-//{
-//	int a[4][5] = { 1,2,3,4,5,6,7,8,9,10 };
-//	for (int i = 0; i < 4; i++) {
-//		for (int j = 0; j < 5; j++)
-//			printf("A[%d][%d]= %d ", i, j, a[i][j]);
-//		printf("\n");
-//	}
-//}
-//void LuuYchu()
-//{
-//	char b[3][4] = { 'x','y','z','a','b','c' };
-//	for (int i = 0; i < 3; i++) {
-//		for (int j = 0; j < 4; j++)
-//			printf("A[%d][%d]= %c ", i, j, b[i][j]);
-//		printf("\n");
-//	}
-//}
-//void main()
-//{
-//	int n;
-//	scanf_s("%d", &n);
-//	switch (n)
-//	{
-//	case 1:
-//		LuuYso(); break;
-//	case 2:
-//		LuuYchu(); break;
-//	default: exit(0);
-//		break;
-//	}
-//}
+#include <stdio.h>
+#include <conio.h>
+void LuuYso()
+{
+	int a[4][5] = { 1,2,3,4,5,6,7,8,9,10 };
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 5; j++)
+			printf("A[%d][%d]= %d ", i, j, a[i][j]);
+		printf("\n");
+	}
+}
+void LuuYchu()
+{
+	char b[3][4] = { 'x','y','z','a','b','c' };
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 4; j++)
+			printf("A[%d][%d]= %c ", i, j, b[i][j]);
+		printf("\n");
+	}
+}
+void main()
+{
+	int n;
+	scanf_s("%d", &n);
+	switch (n)
+	{
+	case 1:
+		LuuYso(); break;
+	case 2:
+		LuuYchu(); break;
+	default: exit(0);
+		break;
+	}
+}
