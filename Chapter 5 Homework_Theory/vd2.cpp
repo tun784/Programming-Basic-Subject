c2b7b142f1ed57cea4fa7506c574775f4de6fd95
Nguyen Hoang Tuan <116370663+tun784@users.noreply.github.com>
2023-07-07 15:46:27 +0700

    Update vd2.cpp

--- Chapter 5 Homework_Theory/vd2.cpp
+++ Chapter 5 Homework_Theory/vd2.cpp
@@ -1,8 +1,8 @@
-//#include <stdio.h>
-//
-//int main() {
-//	int matrix[2][3] = { {1, 4, 2}, {3, 6, 8} };
-//	printf("%d", matrix[0][2]);
-//
-//	return 0;
-//}
+#include <stdio.h>
+
+int main() {
+	int matrix[2][3] = { {1, 4, 2}, {3, 6, 8} };
+	printf("%d", matrix[0][2]);
+
+	return 0;
+}
