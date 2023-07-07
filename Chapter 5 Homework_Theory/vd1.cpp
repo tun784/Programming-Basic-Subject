cd34adf76ecd9322f79b753e6d08163a77df1c79
Nguyen Hoang Tuan <116370663+tun784@users.noreply.github.com>
2023-07-07 15:46:13 +0700

    Update vd1.cpp

--- Chapter 5 Homework_Theory/vd1.cpp
+++ Chapter 5 Homework_Theory/vd1.cpp
@@ -1,31 +1,31 @@
-//#include<stdio.h>
-//
-//void NhapMaTran(int a[][100], int m, int n)
-//{
-//    for (int i = 0; i < m; i++)
-//        for (int j = 0; j < n; j++)
-//        {
-//            printf("A[%d][%d] = ", i, j);
-//            scanf_s("%d", &a[i][j]);
-//        }
-//}
-//
-//void XuatMaTran(int a[][100], int m, int n)
-//{
-//    for (int i = 0; i < m; i++)
-//    {
-//        for (int j = 0; j < n; j++)
-//            printf("%d\t", a[i][j]);
-//        printf("\n");
-//    }
-//}
-//
-//int main() {
-//    int a[100][100];
-//    int m, n;
-//    printf("nhap so hang n = "); scanf_s("%d", &n);
-//    printf("nhap so cot m = "); scanf_s("%d", &m);
-//    printf("nhap vao ma tran:\n");
-//    NhapMaTran(a, m, n);
-//    XuatMaTran(a, m, n);
-//}
+#include<stdio.h>
+
+void NhapMaTran(int a[][100], int m, int n)
+{
+   for (int i = 0; i < m; i++)
+       for (int j = 0; j < n; j++)
+       {
+           printf("A[%d][%d] = ", i, j);
+           scanf_s("%d", &a[i][j]);
+       }
+}
+
+void XuatMaTran(int a[][100], int m, int n)
+{
+   for (int i = 0; i < m; i++)
+   {
+       for (int j = 0; j < n; j++)
+           printf("%d\t", a[i][j]);
+       printf("\n");
+   }
+}
+
+int main() {
+   int a[100][100];
+   int m, n;
+   printf("nhap so hang n = "); scanf_s("%d", &n);
+   printf("nhap so cot m = "); scanf_s("%d", &m);
+   printf("nhap vao ma tran:\n");
+   NhapMaTran(a, m, n);
+   XuatMaTran(a, m, n);
+}
